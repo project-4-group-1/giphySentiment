273ef6dd7d3e7f2d4cbf0151b73f4f1fc8dde3cc
qiqiliau <qiqi.liau@gmail.com>
2021-08-04 17:16:11 -0400

    gif results styling, minor tweaks to header

--- src/App.js
+++ src/App.js
@@ -37,19 +37,20 @@
 import InputOutput from './InputOutput';
 import Button from './Buttons';
 import { useRef } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   // create reference to specific scroll locations on the virtual DOM
   const topOfPage = useRef(null);
 
   return (
-    <div ref={topOfPage}>
-      <main>
+    <Router>
+      <div ref={topOfPage}>
         <InputOutput />
         {/* For each button, pass the scroll reference as props*/}
         <Button target={topOfPage} direction={'up'} />{' '}
-      </main>
-    </div>
+      </div>
+    </Router>
   );
 }
 
