ac7eec466f813d72db60f1c166b985cbe8947353
qiqi.liau <qiqi.liau@gmail.com>
2023-07-24 12:38:33 -0400

    update credits

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
       <footer>
         <p>
-          Created by Accacia Benjamin, Michael Li, QiQi Liau, Shaurya Dubey at
+          Created by Accacia Benjamin, Michael Li, Shaurya Dubey at
           <a href="https://junocollege.com"> Juno College</a> © 2021.
         </p>
       </footer>
